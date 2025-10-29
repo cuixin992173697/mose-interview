f9c317c4364e29213f1db44e259bcb34710ea4ac
cuixin <992173697@qq.com>
2025-10-29 13:48:55 +0800

    [feat](算法): 最长回文子串

---
+++ 常考算法题目/12.无重复最长子串.js
@@ -0,0 +1,15 @@
+function lengthOfLongestSubstring(s) {
+    const set = new Set();
+    let left = 0, maxLen = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+}
